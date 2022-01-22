e92681942b8499f3a4a0563e12d2611978f3d0e2
Dae Hoon Kim <hunimoda@gmail.com>
2022-01-22 10:22:18 +0900

    Added touch event listener: now can draw on mobile

--- app.js
+++ app.js
@@ -1,53 +1,74 @@
 let isNowPainting = false;
 
 // CANVAS *****************************************************
-const canvas = document.getElementById('js-canvas');
+const canvas = document.getElementById("js-canvas");
 canvas.width = 512;
 canvas.height = 512;
 
-canvas.addEventListener('mousemove', onCanvasMouseMove);
-canvas.addEventListener('mousedown', startPainting);
-canvas.addEventListener('mouseup', stopPainting);
-canvas.addEventListener('mouseenter', onCanvasMouseEnter);
-canvas.addEventListener('contextmenu', onCanvasContextMenu);
+canvas.addEventListener("mousemove", onCanvasMouseMove);
+canvas.addEventListener("touchmove", onCanvasTouchMove);
+canvas.addEventListener("mousedown", startPainting);
+canvas.addEventListener("touchstart", startPainting);
+canvas.addEventListener("mouseup", stopPainting);
+canvas.addEventListener("touchend", stopPainting);
+canvas.addEventListener("mouseenter", onCanvasMouseEnter);
+canvas.addEventListener("contextmenu", onCanvasContextMenu);
 
 function onCanvasMouseMove(event) {
-    const x = event.offsetX;
-    const y = event.offsetY;
+	const x = event.offsetX;
+	const y = event.offsetY;
 
-    if (isNowPainting) {
-        ctx.lineTo(x, y);
-        ctx.stroke();
-    }
+	if (isNowPainting) {
+		ctx.lineTo(x, y);
+		ctx.stroke();
+	}
+}
+
+function onCanvasTouchMove(event) {
+	const touch = event.touches[0];
+	const {
+		clientX,
+		clientY,
+		target: { offsetTop, offsetLeft },
+	} = touch;
+
+	const x = clientX - offsetLeft;
+	const y = clientY - offsetTop;
+
+	if (isNowPainting) {
+		ctx.lineTo(x, y);
+		ctx.stroke();
+	}
 }
 
 function startPainting(event) {
-    const x = event.offsetX;
-    const y = event.offsetY;
+	event.preventDefault();
 
-    ctx.beginPath();
-    isNowPainting = true;
+	const x = event.offsetX;
+	const y = event.offsetY;
+
+	ctx.beginPath();
+	isNowPainting = true;
 }
 
 function stopPainting(event) {
-    isNowPainting = false;
+	isNowPainting = false;
 }
 
 function onCanvasMouseEnter(event) {
-    if (isNowPainting) {
-        startPainting(event);
-    }
+	if (isNowPainting) {
+		startPainting(event);
+	}
 }
 
 function onCanvasContextMenu(event) {
-    event.preventDefault();
-    console.log('right click');
+	event.preventDefault();
+	console.log("right click");
 }
 //**********************************************************
 
-
 // CONTEXT *************************************************
-const ctx = canvas.getContext('2d');
+const ctx = canvas.getContext("2d");
 ctx.strokeStyle = "#2c2c2c";
 ctx.lineWidth = 3;
 
@@ -55,64 +76,58 @@ ctx.fillStyle = "#fff";
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 //**********************************************************
 
-
 // RANGE ***************************************************
-const thickness = document.getElementById('js-range');
-thickness.addEventListener('input', onThicknessChange);
+const thickness = document.getElementById("js-range");
+thickness.addEventListener("input", onThicknessChange);
 
 function onThicknessChange(event) {
-    ctx.lineWidth = thickness.value;
+	ctx.lineWidth = thickness.value;
 }
 //**********************************************************
 
-
 // COLOR PICK **********************************************
-const colors = document.querySelectorAll('.controls__color');
-colors.forEach(color => {
-    color.addEventListener('mouseup', onColorPickMouseUp);
+const colors = document.querySelectorAll(".controls__color");
+colors.forEach((color) => {
+	color.addEventListener("mouseup", onColorPickMouseUp);
 });
-colors.forEach(color => {
-    color.addEventListener('mousedown', onColorPickMouseDown);
+colors.forEach((color) => {
+	color.addEventListener("mousedown", onColorPickMouseDown);
 });
 
 function onColorPickMouseUp(event) {
-    ctx.strokeStyle = event.target.style.backgroundColor;
+	ctx.strokeStyle = event.target.style.backgroundColor;
 }
 
 function onColorPickMouseDown(event) {
-    if (!paintMode) {
-        ctx.fillStyle = event.target.style.backgroundColor;
-        ctx.fillRect(0, 0, canvas.width, canvas.height);
-    }
+	if (!paintMode) {
+		ctx.fillStyle = event.target.style.backgroundColor;
+		ctx.fillRect(0, 0, canvas.width, canvas.height);
+	}
 }
 //**********************************************************
-
 
 // MODE ****************************************************
 let paintMode = true;
-const modeToggler = document.getElementById('js-mode');
-modeToggler.addEventListener('click', onModeTogglerClick);
+const modeToggler = document.getElementById("js-mode");
+modeToggler.addEventListener("click", onModeTogglerClick);
 
 function onModeTogglerClick(event) {
-    if (paintMode)
-        modeToggler.innerText = 'Paint';
-    else
-        modeToggler.innerText = 'Fill';
-    paintMode = !paintMode;
+	if (paintMode) modeToggler.innerText = "Paint";
+	else modeToggler.innerText = "Fill";
+	paintMode = !paintMode;
 }
 //**********************************************************
 
-
 // SAVE ****************************************************
-const saveBtn = document.getElementById('js-save');
-saveBtn.addEventListener('click', onSaveBtnClick);
+const saveBtn = document.getElementById("js-save");
+saveBtn.addEventListener("click", onSaveBtnClick);
 
 function onSaveBtnClick(event) {
-    const image = canvas.toDataURL('image/png');
-    const link = document.createElement('a');
+	const image = canvas.toDataURL("image/png");
+	const link = document.createElement("a");
 
-    link.setAttribute('href', image);
-    link.setAttribute('download', 'my-image.png');
-    console.log(link);
-    link.click();
+	link.setAttribute("href", image);
+	link.setAttribute("download", "my-image.png");
+	console.log(link);
+	link.click();
 }
